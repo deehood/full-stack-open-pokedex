75c11a1258cafb8116faaf80d7e6283c85c3332c
deehood <dee.hood@gmail.com>
2022-11-24 16:19:38 +0000

    config fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
     "jest/globals": true,
+    "cypress/globals": true,
+    commonjs: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
